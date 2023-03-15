fdb3c88416fa3a01236c56c06ce96cd47c46127d
Thibaut Paumard <thibaut.paumard@obspm.fr>
2023-03-15 15:03:24 +0100

    Improve example script (two streamers, higher noise)

--- examples/streamers.py
+++ examples/streamers.py
@@ -1,13 +1,14 @@
 """Create and fit mock data with a single streamer
 
-The object is a thin filament with velocity gradient. Line width is
-anti-correlated with line flux.
+The object is made of two thin filaments with velocity gradient. One
+is brighter than the other, with SNR above one only for the brighter
+filament (here SNR= peak/stddev). Line width is anti-correlated with
+line flux.
 
 This script create then fits fake data.
 
 The conclusion is that, in the absence of regularization, the fit is
-OK for SNR>1 and does not work for SNR<=1 (Here: SNR = peak/stddev).
-
+OK for SNR>1 and does not work for SNR<=1.
 """
 
 
@@ -40,22 +41,28 @@ def add_noise(cube, sigma=0.02):
 nx, ny, nz = 16, 16, 21
 alpha=np.linspace(-ny/2, ny/2, ny)[np.newaxis, :]*np.ones((nx, ny))
 delta=np.linspace(-nx/2, nx/2, nx)[:, np.newaxis]*np.ones((nx, ny))
-I=gauss(alpha+delta, 1, 0, 10)[0]*gauss(alpha-delta, 1, 0, 2)[0]
+I=gauss(alpha+delta, 1, 0, 10)[0] \
+    * (  gauss(alpha-delta, 1, 4, 2)[0]
+       + gauss(alpha-delta, 0.25, -4, 2)[0])
 v=2*(alpha+delta)
 dv=(2-I)*5
 if DEBUG:
     plt.imshow(I)
+    plt.title("'True' flux")
     plt.show()
     plt.imshow(v)
+    plt.title("'True' velocity")
     plt.show()
     plt.imshow(dv)
+    plt.title("'True' line width")
     plt.show()
 
 # Model we want to test
 w0=2.166120e-6
-dw=1e-9
+dw=5e-11
+Dw=dw*(nz-1)
 profile = DopplerLines(w0, profile=ngauss)
-waxis = np.linspace(w0-dw/2, w0+dw/2, nz)
+waxis = np.linspace(w0-Dw/2, w0+Dw/2, nz)
 model = CubeModel(profile=profile, profile_xdata=waxis, regularisation=None)
 vaxis = (waxis-w0)/w0*profile.light_speed
 
@@ -63,13 +70,19 @@ vaxis = (waxis-w0)/w0*profile.light_speed
 xreal=np.transpose(np.asarray([I, v, dv]), (1, 2, 0))
 reality=model.model(xreal)
 
+# Sigma of errors to add to "true" cube to get "observational" data
+sigma = 0.2*np.max(reality)
+model.data = add_noise(reality, sigma)
+
 # Initial guess for fit. Can be 1D or 3D.
 xtest_1d=[np.max(I), 0., np.std(v)]
 xtest = np.ones((nx, ny, 1)) * xtest_1d
 
 if DEBUG:
-    plt.plot((waxis-w0)/w0*profile.light_speed, reality[nx//2,ny//2,:], label="central spectrum")
-    plt.plot((waxis-w0)/w0*profile.light_speed, reality[0,0,:], label="lower left spectrum")
+    plt.plot((waxis-w0)/w0*profile.light_speed, reality[6,10, :], label="bright spectrum (true)")
+    plt.plot((waxis-w0)/w0*profile.light_speed, model.data[6,10,:], label="bright spectrum (data)")
+    plt.plot((waxis-w0)/w0*profile.light_speed, reality[10,6,:], label="faint spectrum (true)")
+    plt.plot((waxis-w0)/w0*profile.light_speed, model.data[10,6,:], label="faint spectrum (data)")
     plt.plot((waxis-w0)/w0*profile.light_speed, profile(waxis, *xtest_1d)[0], label="first guess")
     plt.plot((waxis-w0)/w0*profile.light_speed, np.sum(np.sum(reality, axis=1), axis=0)/nx,
              label=f"integral spectrum/{nx}")
@@ -77,7 +90,7 @@ if DEBUG:
     plt.show()
 
 # Sigma of errors to add to "true" cube to get "observational" data
-sigma = 0.05*np.max(reality)
+sigma = 0.2*np.max(reality)
 model.data = add_noise(reality, sigma)
 
 # Do fit
@@ -92,21 +105,34 @@ chi2=np.sum(((model.data-model_cube)/sigma)**2)/(model.data.size-res_x.size)
 print(f"reduced chi2 == {chi2}")
 
 def plot3(i, j):
-    plt.plot(vaxis, reality[i, j, :])
-    plt.plot(vaxis, model.data[i, j, :])
-    plt.plot(vaxis, model_cube[i, j, :])
+    plt.plot(vaxis, reality[i, j, :], label=f"true spectrum at {j} {i}")
+    plt.plot(vaxis, model.data[i, j, :], label=f"data spectrum at {j} {i}")
+    plt.plot(vaxis, model_cube[i, j, :], label=f"fitted spectrum at {j} {i}")
+    plt.legend()
     plt.show()
 
 if DEBUG:
     # Display fit for central pixel
-    plot3(nx//2, ny//2)
+    plot3(6, 10)
     # Display fit for lower left pixel
-    plot3(0, 0)
+    plot3(10, 6)
+
+# Display intensity map
+plt.imshow(res_x[:,:,0])
+plt.title("fitted flux")
+plt.show()
 
 # Display velocity map
 plt.imshow(res_x[:,:,1])
+plt.title("fitted velocity")
+plt.show()
+
+# Display width map
+plt.imshow(res_x[:,:,2])
+plt.title("fitted width")
 plt.show()
 
 # Display SNR map (peak/sigma)
 plt.imshow(np.max(reality, axis=2)>sigma)
+plt.title("Where true peak value > noise level")
 plt.show()
