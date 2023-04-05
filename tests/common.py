a63fcd920a167d5d8e0dfec40c99828e6d04bcee
brule <julien.brule@obspm.fr>
2023-04-05 15:48:30 +0200

    add licensing information for the tests

--- tests/common.py
+++ tests/common.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+#    Copyright (C) 2023  Thibaut Paumard <thibaut.paumard@obspm.fr>
+#            Julien Brulé
+#            Damien Gratadour
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import unittest
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +42,7 @@ class Test1DModel(unittest.TestCase):
             x = np.asarray(x, dtype=np.float64)
         nterms = a.size
         jac = np.zeros(x.shape + (nterms,))
-    
+
         y0, jac0=f(x, *a)
         jac = numerical_jacobian(lambda x, *a: f(x, *a)[0], x, *a)
 
--- tests/test_dopplerlines.py
+++ tests/test_dopplerlines.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+#    Copyright (C) 2023  Thibaut Paumard <thibaut.paumard@obspm.fr>
+#            Julien Brulé
+#            Damien Gratadour
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import os
 from scipy import optimize
 from .common import *
@@ -16,7 +35,7 @@ class TestDopplerlines(Test1DModel):
         self.lineobj1 = DopplerLines(2.166120)
         self.lineobj2 = DopplerLines((2.166120, 2.155))
         Test1DModel.__init__(self, methodName)
-        
+
     def test_dopplerlines_jacobian(self):
         self.check_jacobian(self.lineobj1, self.waxis, 1., 0., 50., reltol=1e-2, diftol=1e-9)
         self.check_jacobian(self.lineobj2, self.waxis, 1.2, 0.5, 25., 100., reltol=1e-2)
