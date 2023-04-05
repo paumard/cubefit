1421a0fe6dde49e2940897fe3cff383e1bbdd84f
brule <julien.brule@obspm.fr>
2023-04-05 17:07:30 +0200

    correcting authors

--- tests/test_cubemodel.py
+++ tests/test_cubemodel.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 #    Copyright (C) 2023  Thibaut Paumard <thibaut.paumard@obspm.fr>
 #            Julien Brulé
-#            Damien Gratadour
 #
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
